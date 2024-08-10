2bc7480c2700b4d5855a2071f4faebc0de98237d
Matt Page <94657526+mattbillpage@users.noreply.github.com>
2024-08-10 14:50:07 +1000

    Create Csci318gpApplication.java

---
+++ src/main/java/com/example/csci318gp/Csci318gpApplication.java
@@ -0,0 +1,13 @@
+package com.example.csci318gp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Csci318gpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Csci318gpApplication.class, args);
+	}
+
+}
