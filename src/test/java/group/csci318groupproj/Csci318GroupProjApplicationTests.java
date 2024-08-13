9137c1782085a06078a83825518b6719f1d70b7d
Brayden <bcowell04@gmail.com>
2024-08-13 13:54:38 +1000

    Rebuilt Project in Maven

---
+++ src/test/java/group/csci318groupproj/Csci318GroupProjApplicationTests.java
@@ -0,0 +1,13 @@
+package group.csci318groupproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Csci318GroupProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
