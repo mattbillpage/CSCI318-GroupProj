fe95407bd81d973f37e263da59c2e6d0a182d29a
aks805 <150407434+aks805@users.noreply.github.com>
2024-09-05 14:26:48 +1000

    added functionality to create customers

--- Customer-Service/src/main/java/csci318/group10/customerservice/Controller/CustomerController.java
+++ Customer-Service/src/main/java/csci318/group10/customerservice/Controller/CustomerController.java
@@ -1,28 +1,31 @@
 package csci318.group10.customerservice.Controller;
 
-
 import csci318.group10.customerservice.Model.Customer;
 import csci318.group10.customerservice.Service.CustomerService;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/customer")
 public class CustomerController {
-    private final CustomerService customerService;
+  private final CustomerService customerService;
 
-    CustomerController(CustomerService customerService) {
-        this.customerService = customerService;
-    }
+  CustomerController(CustomerService customerService) {
+    this.customerService = customerService;
+  }
 
-    @GetMapping("/all")
-    List<Customer> allCustomers() {
-        return customerService.getAllCustomers();
-    }
+  @GetMapping("/all")
+  List<Customer> allCustomers() {
+    return customerService.getAllCustomers();
+  }
 
-    @GetMapping("/{id}")
-    Customer getCustomer(@PathVariable int id) {
-        return customerService.getCustomerById(id);
-    }
+  @GetMapping("/{id}")
+  Customer getCustomer(@PathVariable int id) {
+    return customerService.getCustomerById(id);
+  }
+
+  @PostMapping("/add")
+  Customer createCustomer(@RequestBody Customer newCustomer) {
+    return customerService.addCustomer(newCustomer);
+  }
 }
--- Customer-Service/src/main/java/csci318/group10/customerservice/Service/CustomerService.java
+++ Customer-Service/src/main/java/csci318/group10/customerservice/Service/CustomerService.java
@@ -1,30 +1,32 @@
 package csci318.group10.customerservice.Service;
 
+import csci318.group10.customerservice.Model.Customer;
 import csci318.group10.customerservice.Repository.CustomerRepository;
+import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import csci318.group10.customerservice.Model.Customer;
 
-import java.util.List;
-
-@Service
+@Service // what does this do
 public class CustomerService {
 
-    private final CustomerRepository customerRepository;
-    private final RestTemplate restTemplate;
+  private final CustomerRepository customerRepository;
+  private final RestTemplate restTemplate;
 
-    public CustomerService(CustomerRepository customerRepository, RestTemplate restTemplate) {
-        this.customerRepository = customerRepository;
-        this.restTemplate = restTemplate;
-    }
+  public CustomerService(CustomerRepository customerRepository,
+                         RestTemplate restTemplate) {
+    this.customerRepository = customerRepository;
+    this.restTemplate = restTemplate;
+  }
 
-    public List<Customer> getAllCustomers() {
-        return customerRepository.findAll();
-    }
+  public List<Customer> getAllCustomers() {
+    return customerRepository.findAll();
+  }
 
-    public Customer getCustomerById(int id) {
-        return customerRepository.findById(id).orElse(null);
-    }
+  public Customer getCustomerById(int id) {
+    return customerRepository.findById(id).orElse(null);
+  }
 
-
+  public Customer addCustomer(Customer newCustomer) {
+    return customerRepository.save(newCustomer);
+  }
 }
